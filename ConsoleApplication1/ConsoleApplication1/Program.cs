9441895b0c3a528749c29b3170d3e88dd996d0bd
IrinaPotapova <ira_8_pot@mail.ru>
2015-10-27 20:21:49 +0500

    заготовка 2 лабы

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+      
+        private void serialized()
+        {
+           
+        }
+
+        private void deserialized()
+        {          
+           
+        }
+        private void MLV()
+        {
+        
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.serialized();
+            p.deserialized();
+            p.MLV();
+        }
+
+    }
+}
